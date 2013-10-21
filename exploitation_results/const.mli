a81fc351fe88ec173de2d9fd4445246122667eba
Gaupy <https://github.com/gaupy>
2013-10-21 11:16:23 +0200

    Files to exploit results.

---
+++ exploitation_results/const.mli
@@ -0,0 +1,19 @@
+type parameter = {
+  rmax : int;
+  tree_type : int;
+  size_of_tree : int;
+  number_of_speeds : int;
+  max_speed : int;
+  regularity_speed : int;
+  number_of_tests : int
+ }
+
+val default : parameter
+
+(* val lambda0 : float *)
+(* val fmax : float *)
+(* val fr : float *)
+(* val n : int *)
+(* val ratio_deadlineMAX : float *)
+(* val bound : float *)
+(* val nbrHeur : int *)
