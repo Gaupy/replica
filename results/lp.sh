cb206c22686d0994d4ce77ea09b2b270cbf765da
Gaupy <https://github.com/gaupy>
2013-10-21 11:16:40 +0200

    Files to obtain results.

---
+++ results/lp.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+cd ..
+echo "$1
+$2
+$3
+$4
+$5
+$6
+$7" > conf
+./comp conf
+cd results
+gcc ../LP/lp.c -o test -lglpk -fno-stack-protector 
+for r in `seq 1 $7`; do
+	for s in `seq 1 $3`; do
+		./test $3 $s $r >> result_$3.score
+	done
+done
+rm ../conf
