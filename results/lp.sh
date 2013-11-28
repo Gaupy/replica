bcafe8372e4e541f6a725e6c2b15fe4b576f1cb1
Gaupy <https://github.com/gaupy>
2013-11-28 13:08:14 +0100

    Parallelization of script for fater simulations.

--- results/lp.sh
+++ results/lp.sh
@@ -1,19 +1,18 @@
 #!/bin/sh
 cd ..
-echo "$1
-$2
-$3
-$4
-$5
-$6
-$7" > conf
+echo "$1\n$2\n$3\n$4\n$5\n$6\n$7" > conf
 make opt
 ./comp conf
 cd results
 gcc ../LP/lp.c -o test -lglpk -fno-stack-protector 
 for r in `seq 1 $7`; do
 	for s in `seq 1 $3`; do
-		./test $3 $s $r >> result_$3.score
+		./test $3 $s $r >> result_$3_$s_$r.temp &
 	done
 done
-rm ../conf
+for r in `seq 1 $7`; do
+	for s in `seq 1 $3`; do
+		cat result_$3_$s_$r.temp >> result_$3_$4_$6.score;
+	done
+done
+rm ../conf *.temp
