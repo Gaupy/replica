2db89e9b0b181fa100739e2f707401420c03990a
Gaupy <https://github.com/gaupy>
2014-01-27 23:15:08 +0100

    Modified how the results from cplex are extracted. Added case 0 equal to case 1 where we compare to the optimal solution for bigger trees.

--- scripts/full_script.sh
+++ scripts/full_script.sh
@@ -15,22 +15,22 @@ make opt
 cd results
 gcc ../LP/new_lp.c -o test -lglpk -fno-stack-protector 
 case $9 in
-	1 )
+	0 )
 	for nodes in `seq 1 $3`; do
 		for iter in `seq 1 $8`; do
 			./test ${nodes} $5 $9 ${iter}
 			rm size=${nodes}_idle=${5}_expe=$9_iter=${iter}.dat
-			if [ "${nodes}" -lt 25 ]; then
-				cplex -c "r pbm_size=${nodes}_idle=$5_expe=$9_iter=${iter}_general.lp" "opt"|grep "Objective ="| cut -d " " -f 10 >> result_${nodes}_${5}_${9}_${iter}.temp
+			if [ "${nodes}" -lt 31 ]; then
+				cplex -c "r pbm_size=${nodes}_idle=$5_expe=$9_iter=${iter}_general.lp" "opt"|grep "Objective ="| cut -d "=" -f 2 >> result_${nodes}_${5}_${9}_${iter}.temp
 			else
-				echo "\n" >> result_${nodes}_${5}_${9}_${iter}.temp
+				echo "" >> result_${nodes}_${5}_${9}_${iter}.temp
 			fi
 			rm pbm_size=${nodes}_idle=$5_expe=$9_iter=${iter}_general.lp
 		done
 	done
 	for nodes in `seq 1 $3`; do
 		for iter in `seq 1 $8`; do
-			cat result_${nodes}_${5}_${9}_${iter}.temp >> result_$3_$4_$7_$9.score
+			cat result_${nodes}_${5}_${9}_${iter}.temp >> result_$3_$4_$5_$7_$9.score
 			rm result_${nodes}_${5}_${9}_${iter}.temp
 		done
 	done
@@ -38,15 +38,81 @@ case $9 in
 	cd ../exploitation_results
 	echo "$1\n$2\n$3\n$4\n$5\n$6\n$7\n$8\n$9" > conf
 	make
-	./comp conf < ../results/result_$3_$4_$7_$9.score
+	./comp conf < ../results/result_$3_$4_$5_$7_$9.score
+	gnuplot maxnodes_$3_rmax=$1_treetype=$2_nspeed=$4_maxspeed=$6_typespeed=$7_static=$5.p
+	;;
+	1 )
+	for nodes in `seq 1 $3`; do
+		for iter in `seq 1 $8`; do
+			./test ${nodes} $5 $9 ${iter}
+			rm size=${nodes}_idle=${5}_expe=$9_iter=${iter}.dat
+			if [ "${nodes}" -lt 26 ]; then
+				cplex -c "r pbm_size=${nodes}_idle=$5_expe=$9_iter=${iter}_general.lp" "opt"|grep "Objective ="| cut -d "=" -f 2 >> result_${nodes}_${5}_${9}_${iter}.temp
+			else
+				echo "" >> result_${nodes}_${5}_${9}_${iter}.temp
+			fi
+			rm pbm_size=${nodes}_idle=$5_expe=$9_iter=${iter}_general.lp
+		done
+	done
+	for nodes in `seq 1 $3`; do
+		for iter in `seq 1 $8`; do
+			cat result_${nodes}_${5}_${9}_${iter}.temp >> result_$3_$4_$5_$7_$9.score
+			rm result_${nodes}_${5}_${9}_${iter}.temp
+		done
+	done
+	rm *.log
+	cd ../exploitation_results
+	echo "$1\n$2\n$3\n$4\n$5\n$6\n$7\n$8\n$9" > conf
+	make
+	./comp conf < ../results/result_$3_$4_$5_$7_$9.score
 	gnuplot maxnodes_$3_rmax=$1_treetype=$2_nspeed=$4_maxspeed=$6_typespeed=$7_static=$5.p
 #	echo "You have tried an expe_number that is not yet implemented."
 	;;
 	2 )
-	echo "You have tried an expe_number that is not yet implemented."
+	for iter in `seq 1 $8`; do
+		./test ${3} $5 $9 ${iter}
+		rm size=${3}_idle=${5}_expe=$9_iter=${iter}.dat
+		if [ "${3}" -lt 26 ]; then
+			cplex -c "r pbm_size=${3}_idle=$5_expe=$9_iter=${iter}_general.lp" "opt"|grep "Objective ="| cut -d "=" -f 2 >> result_${3}_${5}_${9}_${iter}.temp
+		else
+			echo "" >> result_${3}_${5}_${9}_${iter}.temp
+		fi
+		rm pbm_size=${3}_idle=$5_expe=$9_iter=${iter}_general.lp
+	done
+	for iter in `seq 1 $8`; do
+		cat result_${3}_${5}_${9}_${iter}.temp >> result_$3_$4_$7_$9.score
+		rm result_${3}_${5}_${9}_${iter}.temp
+	done
+	rm *.log
+	cd ../exploitation_results
+	echo "$1\n$2\n$3\n$4\n$5\n$6\n$7\n$8\n$9" > conf
+	make
+	./comp conf < ../results/result_$3_$4_$7_$9.score
+	gnuplot static_energy_$5_nodes=$3_rmax=$1_treetype=$2_nspeed=$4_maxspeed=$6_typespeed=$7.p
 	;;
 	3 )
-	echo "You have tried an expe_number that is not yet implemented."
+	for static in `seq 0 1000 $5`; do
+		for iter in `seq 1 $8`; do
+			./test ${3} $5 $9 ${iter}
+			rm size=${3}_idle=${5}_expe=$9_iter=${iter}.dat
+			if [ "${3}" -lt 26 ]; then
+				cplex -c "r pbm_size=${3}_idle=$5_expe=$9_iter=${iter}_general.lp" "opt"|grep "Objective ="| cut -d " " -f 10 >> result_${3}_${5}_${9}_${iter}.temp
+			else
+				echo "" >> result_${3}_${5}_${9}_${iter}.temp
+			fi
+			rm pbm_size=${3}_idle=$5_expe=$9_iter=${iter}_general.lp
+		done
+		for iter in `seq 1 $8`; do
+			cat result_${3}_${5}_${9}_${iter}.temp >> result_$3_$4_$5_$7_$9.score
+			rm result_${3}_${5}_${9}_${iter}.temp
+		done
+	done
+	rm *.log
+	cd ../exploitation_results
+	echo "$1\n$2\n$3\n$4\n$5\n$6\n$7\n$8\n$9" > conf
+	make
+	./comp conf < ../results/result_$3_$4_$5_$7_$9.score
+	gnuplot static_energy_$5_nodes=$3_rmax=$1_treetype=$2_nspeed=$4_maxspeed=$6_typespeed=$7.p
 	;;
 	* ) 
 	echo "You have tried an expe_number that is not yet implemented."
---
+++ scripts/plots.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+case $9 in
+	0 )
+	cd ../exploitation_results
+	echo "$1\n$2\n$3\n$4\n$5\n$6\n$7\n$8\n$9" > conf
+	make
+	./comp conf < ../results/result_$3_$4_$5_$7_$9.score
+	gnuplot maxnodes_$3_rmax=$1_treetype=$2_nspeed=$4_maxspeed=$6_typespeed=$7_static=$5.p
+#	echo "You have tried an expe_number that is not yet implemented."
+	;;
+	1 )
+	cd ../exploitation_results
+	echo "$1\n$2\n$3\n$4\n$5\n$6\n$7\n$8\n$9" > conf
+	make
+	./comp conf < ../results/result_$3_$4_$5_$7_$9.score
+	gnuplot maxnodes_$3_rmax=$1_treetype=$2_nspeed=$4_maxspeed=$6_typespeed=$7_static=$5.p
+#	echo "You have tried an expe_number that is not yet implemented."
+	;;
+	2 )
+	cd ../exploitation_results
+	echo "$1\n$2\n$3\n$4\n$5\n$6\n$7\n$8\n$9" > conf
+	make
+	./comp conf < ../results/result_$3_$4_$5_$7_$9.score
+	gnuplot static_energy_$5_nodes=$3_rmax=$1_treetype=$2_nspeed=$4_maxspeed=$6_typespeed=$7.p
+	;;
+	3 )
+	echo "You have tried an expe_number that is not yet implemented."
+	;;
+	* ) 
+	echo "You have tried an expe_number that is not yet implemented."
+	;;
+esac
