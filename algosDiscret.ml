426d7f0bd8b19134049d73f5b13054de366ab5f4
Gaupy <https://github.com/gaupy>
2014-01-27 15:41:10 +0100

    Added improved heuristics.

--- algosDiscret.ml
+++ algosDiscret.ml
@@ -5,25 +5,71 @@ open Tools
 open Algos
 
 
-(*Warning, tab_of_speeds should always start with speed 0.*)
+(*Warning, energy should always start with speed 0.*)
 
-let algo_discret id t size_t n tab_of_speeds =
+let algo_discret id t size_t n energy =
    match id with
     | 0 -> 
       let _, t_cont = algo_intelligent t n in 
-      let t_temp = tarbre_to_arbre_int t_cont tab_of_speeds in
+      let t_temp = tarbre_to_arbre_int t_cont energy in
       let matrix = make_prec_matrix_server_tree_int t_temp size_t in
-      let t_discrete = optim_discret 0 (t_temp) tab_of_speeds in
-        (energy_int t_discrete tab_of_speeds, matrix)
+      let t_discrete = optim_discret 0 (t_temp) energy in
+        (energy_int t_discrete energy, matrix)
     | 1 -> 
       let _ ,t_cont = algo_gopi t n in
-      let t_temp = tarbre_to_arbre_int t_cont tab_of_speeds in
+      let t_temp = tarbre_to_arbre_int t_cont energy in
       let matrix = make_prec_matrix_server_tree_int t_temp size_t in
-      let t_discrete = optim_discret 0 (t_temp) tab_of_speeds in
-        (energy_int t_discrete tab_of_speeds, matrix)
-    | _ -> 
+      let t_discrete = optim_discret 0 (t_temp) energy in
+        (energy_int t_discrete energy, matrix)
+    | 2 -> 
       let _ ,t_cont = greedy_7 t n in
-      let t_temp = tarbre_to_arbre_int t_cont tab_of_speeds in
+      let t_temp = tarbre_to_arbre_int t_cont energy in
       let matrix = make_prec_matrix_server_tree_int t_temp size_t in
-      let t_discrete = optim_discret 0 (t_temp) tab_of_speeds in
-        (energy_int t_discrete tab_of_speeds, matrix)
+      let t_discrete = optim_discret 0 (t_temp) energy in
+        (energy_int t_discrete energy, matrix)
+    | 3 -> 
+      let _, t_cont = algo_intelligent t n in 
+      let t_temp = tarbre_to_arbre_int t_cont energy in
+      let matrix = make_prec_matrix_server_tree_int t_temp size_t in
+      let t_discrete = optim_discret 1 (t_temp) energy in
+        (energy_int t_discrete energy, matrix)
+    | 4 -> 
+      let _, t_cont = algo_intelligent t n in 
+      let t_temp = tarbre_to_arbre_int t_cont energy in
+      let matrix = make_prec_matrix_server_tree_int t_temp size_t in
+        (energy_int t_temp energy, matrix)
+    | 5 -> 
+      let _, t_cont = algo_intelligent t n in 
+      let t_temp = tarbre_to_arbre_int t_cont energy in
+      let matrix = make_prec_matrix_server_tree_int t_temp size_t in
+      let t_discrete_v1 = optim_discret 0 (t_temp) energy in
+      let t_discrete_v2 = optim_discret_second_turn 0 t_discrete_v1 energy in
+        (energy_int t_discrete_v2 energy, matrix)
+    | 6 -> 
+      let _, t_cont = algo_intelligent t n in 
+      let t_temp = tarbre_to_arbre_int t_cont energy in
+      let matrix = make_prec_matrix_server_tree_int t_temp size_t in
+      let t_discrete_v1 = optim_discret 0 (t_temp) energy in
+      let t_discrete_v2 = optim_discret_second_turn 1  t_discrete_v1 energy in
+        (energy_int t_discrete_v2 energy, matrix)
+    | 7 -> 
+      let _, t_cont = algo_intelligent t n in 
+      let t_temp = tarbre_to_arbre_int t_cont energy in
+      let matrix = make_prec_matrix_server_tree_int t_temp size_t in
+      let t_discrete_v1 = optim_discret 1 (t_temp) energy in
+      let t_discrete_v2 = optim_discret_second_turn 0 t_discrete_v1 energy in
+        (energy_int t_discrete_v2 energy, matrix)
+    | 8 -> 
+      let _, t_cont = algo_intelligent t n in 
+      let t_temp = tarbre_to_arbre_int t_cont energy in
+      let matrix = make_prec_matrix_server_tree_int t_temp size_t in
+      let t_discrete_v1 = optim_discret 1 (t_temp) energy in
+      let t_discrete_v2 = optim_discret_second_turn 1 t_discrete_v1 energy in
+        (energy_int t_discrete_v2 energy, matrix)
+    | _ -> 
+      let _, t_cont = algo_intelligent t n in 
+      let t_temp = tarbre_to_arbre_int t_cont energy in
+      let matrix = make_prec_matrix_server_tree_int t_temp size_t in
+      let t_discrete = optim_discret 1 (t_temp) energy in
+        (energy_int t_discrete energy, matrix)
+
